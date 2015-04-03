4c8f1321b7c0846adee48057589744ef9248eacb
tunnckoCore <mameto_100@mail.bg>
2015-04-03 20:05:42 +0300

    first commits

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * run-commands <https://github.com/tunnckoCore/run-commands>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var series = require('run-series');
+var parallel = require('run-parallel');
+var asyncRun = require('async-exec-cmd');
+
+
+module.exports = function runCommands() {
+  // body
+};
---
+++ test.js
@@ -0,0 +1,16 @@
+/**
+ * run-commands <https://github.com/tunnckoCore/run-commands>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var test = require('testit');
+var assert = require('assert');
+var runCommands = require('./index');
+
+test('run-commands:', function() {
+  // body
+});
